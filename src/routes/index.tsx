95762f6fce6ce8ea4e8f5aee5a9df279a06be8ee
ybunhong <ybunhon369@gmail.com>
2025-10-23 17:51:34 +0700

    feat: add react router for navigation

---
+++ src/routes/index.tsx
@@ -0,0 +1,10 @@
+import { mainRoute } from "./main";
+import { useRoutes } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+
+export default function Router() {
+  return useRoutes([
+    ...mainRoute,
+    { path: "*", element: <Navigate to="/" replace /> },
+  ]);
+}
---
+++ src/routes/main.tsx
@@ -0,0 +1,39 @@
+import { Outlet } from "react-router-dom";
+import { Suspense } from "react";
+import Home from "../pages/home";
+import Experiences from "../pages/experiences";
+import Contact from "../pages/contact";
+
+export const mainRoute = [
+  {
+    element: (
+      <>
+        <Suspense fallback={<div>Loading</div>}></Suspense>
+        <Outlet />
+      </>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "experiences",
+        element: <Experiences />,
+      },
+
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+      {
+        path: "500",
+        element: <div>500 Error Page</div>,
+      },
+      {
+        path: "404",
+        element: <div>404 Not Found Page</div>,
+      },
+    ],
+  },
+];
