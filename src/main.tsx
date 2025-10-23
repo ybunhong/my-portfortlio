d2c24124d463cb8c765249161af44facd4edbc02
ybunhong <ybunhon369@gmail.com>
2025-10-23 18:16:25 +0700

    fix: route error need _redirects

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,11 @@ import App from "./App.tsx";
 import { ThemeProvider } from "@mui/material/styles";
 import "./index.css";
 import theme from "./shared-theme/theme.ts";
-import { Button } from "@mui/material";
 import { BrowserRouter } from "react-router-dom";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Button variant="contained">default</Button>
       <ThemeProvider theme={theme}>
         <App />
       </ThemeProvider>
