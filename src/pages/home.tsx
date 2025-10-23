ee498ba99a9451dc70fc766ecfc4bc5e3f693c3f
ybunhong <ybunhon369@gmail.com>
2025-10-23 17:56:17 +0700

    feat: build a home page

---
+++ src/pages/home.tsx
@@ -0,0 +1,13 @@
+import HeroBanner from "../components/Hero-Banner";
+import ProjectCard from "../components/project-card";
+import Techbox from "../components/tech-box";
+
+export default function Home() {
+  return (
+    <>
+      <HeroBanner />
+      <ProjectCard />
+      <Techbox />
+    </>
+  );
+}
