5488cb52352d4f264b613cb71a3c990bc523c6f6
ybunhong <ybunhon369@gmail.com>
2025-10-30 20:33:12 +0700

    feat: add padding and bg color

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -5,9 +5,15 @@ import Techbox from "../components/tech-box";
 export default function Home() {
   return (
     <>
-      <HeroBanner />
-      <ProjectCard />
-      <Techbox />
+      <section style={{ background: 'rgba(255,255,255,0.8)', borderRadius: 24, padding: '48px 0', margin: '32px 0' }}>
+        <HeroBanner />
+      </section>
+      <section style={{ background: '#f7f7fa', borderRadius: 24, padding: '48px 0', margin: '32px 0' }}>
+        <ProjectCard />
+      </section>
+      <section style={{ background: 'rgba(255,255,255,0.8)', borderRadius: 24, padding: '48px 0', margin: '32px 0' }}>
+        <Techbox />
+      </section>
     </>
   );
 }
